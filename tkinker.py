f96eefa9b2e0efa013e321936ecd7f0b0d5ecb51
JvHaeckel <joaohaeckel@gmail.com>
2025-05-19 09:34:55 -0300

    MOBI

---
+++ tkinker.py
@@ -0,0 +1,24 @@
+# Converter PDF para excel usando Python 
+
+import pandas as pd
+import tabula
+import tkinter as tk 
+from tkinter import filedialog, messagebox
+
+arquivo = r("")
+dataframe = tabula
+
+
+
+################ Interface gráfica ################
+
+janela = tk.Tk()
+janela.geometry("500x250")
+janela.title("Conversor de Pdf")
+
+# Título maior em negrito
+
+tk.Label(janela, text = )
+
+
+
